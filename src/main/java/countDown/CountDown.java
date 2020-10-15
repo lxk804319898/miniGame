8acbbae2a1f70b2b75cc35a251f7f3a6bd9b4cd6
Liu Zhongyu <seLiuZhongyu@outlook.com>
2020-10-15 11:27:32 +0800

    upd: 倒计时使用 Java 8 新式的 Date Time API 重写，清理了无用代码并整理逻辑

--- src/main/java/countDown/CountDown.java
+++ src/main/java/countDown/CountDown.java
@@ -2,99 +2,73 @@ package countDown;
 
 import javax.swing.*;
 import java.awt.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * 倒计时
  * @author masgak
- * @todo 中午倒计时需求
  */
 public class CountDown {
 
     private JLabel jl0;
-    private JLabel jm0;
 
     private ScheduledThreadPoolExecutor scheduled;
-    
-    private Date stringToDate(String dateStr) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        try {
-            Date date = simpleDateFormat.parse(dateStr);
-            return date;
-        } catch (ParseException e) {
-            jl0.setText("时间格式传入错误,如yyyy-MM-dd HH:mm:ss，" + dateStr);
-            throw new IllegalArgumentException("时间格式传入错误,如yyyy-MM-dd HH:mm:ss，" + dateStr);
-        }
-    }
 
     public void initTime(){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = new Date();
-        timer(simpleDateFormat.format(date)+" "+"18:10:00",simpleDateFormat.format(date)+" "+"11:56:00" , simpleDateFormat.format(date)+" "+"14:00:00");
+        LocalDate today = LocalDate.now();
+        LocalTime offWorkTime = LocalTime.of(18, 10, 0);
+        LocalTime midBreakStart = LocalTime.of(11, 56, 0);
+        LocalTime midBreakEnd = LocalTime.of(14, 0, 0);
+
+        timer(
+            LocalDateTime.of(today, offWorkTime),
+            LocalDateTime.of(today, midBreakStart),
+            LocalDateTime.of(today, midBreakEnd)
+        );
     }
 
-    private void timer(String dateStr ,String dateMid ,String dateMidEnd) {
-        Date end = stringToDate(dateStr);
-        Date mid = stringToDate(dateMid);
-        Date midEnd = stringToDate(dateMidEnd);
-        String endWarn = "已到达下班时间！";
-        String midWarn = "已到达午休时间！";
+    private void timer(LocalDateTime offWorkTime, LocalDateTime midBreakStartTime, LocalDateTime midBreakEndTime) {
         String midEndWarn = "午休时间已过！";
-        scheduled.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                StringBuffer showText = new StringBuffer("<html><br>");
-                // 判断午休
-                long midTime = (mid.getTime() - 1 - System.currentTimeMillis()) / 1000;
-                long midEndTime = (midEnd.getTime() - 1 - System.currentTimeMillis()) / 1000;
-                if(midTime <= 0 && midEndTime <= 0) {
-                    showText.append(midEndWarn + "<br><br>");
-                }
-                if(midTime <= 0 && midEndTime > 0){
-                    long midEndHour = midEndTime / 3600;
-                    long midEndMinute = (midEndTime - midEndHour * 3600) / 60;
-                    long midEndSeconds = midEndTime - midEndHour * 3600 - midEndMinute * 60;
-                    showText.append("距离 午休结束"+ dateMidEnd + "还有<br>" +
-                            midEndHour + "时 " + midEndMinute + "分 " + midEndSeconds + "秒 " +
-                            "<br><br>");
-                }
-                if(midTime > 0){
-                    long midHour = midTime / 3600;
-                    long midMinute = (midTime - midHour * 3600) / 60;
-                    long midSeconds = midTime - midHour * 3600 - midMinute * 60;
-                    showText.append("距离 午休开始"+ dateMid + "还有<br>" +
-                            midHour + "时 " + midMinute + "分 " + midSeconds + "秒 " +
-                            "<br><br>");
-                }
+        scheduled.scheduleAtFixedRate(() -> {
+            LocalDateTime now = LocalDateTime.now();
+            StringBuilder showText = new StringBuilder("<html><br>");
 
-                // 判断下班
-                long time = (end.getTime() - 1 - System.currentTimeMillis()) / 1000;
-                if (time <= 0) {
-                    stopTimer(true);
-                    //showText.append(endWarn + "<br>");
-                    //showText.append("<img src=\"src/resources/CountDownResources/apple.png\" />");
-                }else{
-                    long hour = time / 3600;
-                    long minute = (time - hour * 3600) / 60;
-                    long seconds = time - hour * 3600 - minute * 60;
-                    showText.append("距离 下班"+ dateStr + "还有<br>" +
-                            hour + "时 " + minute + "分 " + seconds + "秒 " +
-                            "</html>");
-                }
-
-
-//                String stringBuilder = "<html><br>距离" + dateStr + "还有<br><br>" +
-//                        hour + "时 " + minute + "分 " + seconds + "秒 " +
-//                        "</html>";
-
-                jl0.setText(showText.toString());
+            if (now.isBefore(midBreakStartTime)) {
+                Duration remaining = Duration.ofSeconds(now.until(midBreakStartTime, ChronoUnit.SECONDS));
+                showText.append("距离午休开始（").append(midBreakStartTime).append("）还有<br>")
+                    .append(formatRemaining(remaining))
+                    .append("<br><br>");
+            } else if (now.isBefore(midBreakEndTime)) {
+                Duration remaining = Duration.ofSeconds(now.until(midBreakEndTime, ChronoUnit.SECONDS));
+                showText.append("距离午休结束（").append(midBreakEndTime).append("）还有<br>")
+                    .append(formatRemaining(remaining))
+                    .append("<br><br>");
+            } else if (now.isBefore(offWorkTime)) {
+                showText.append(midEndWarn).append("<br><br>");
+                Duration remaining = Duration.ofSeconds(now.until(offWorkTime, ChronoUnit.SECONDS));
+                showText.append("距离下班（").append(offWorkTime).append("）还有<br>")
+                    .append(formatRemaining(remaining))
+                    .append("<br><br>");
+            } else {
+                stopTimer(true);
             }
+            System.out.println(showText.toString());
+            jl0.setText(showText.toString());
         }, 0, 1, TimeUnit.SECONDS);
+    }
 
+    private String formatRemaining(Duration remaining) {
+        long remainingTime = remaining.getSeconds();
+        long remainingHours = remainingTime / 3600;
+        long remainingMinutes = (remainingTime % 3600) / 60;
+        long remainingSeconds = (remainingTime % 3600) % 60;
+        return String.format("%d时%d分%d秒", remainingHours, remainingMinutes, remainingSeconds);
     }
 
     /**
