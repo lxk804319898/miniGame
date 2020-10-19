2468057dfedc81188d3ed5e545266a26e4e74fb9
Liu Zhongyu <seLiuZhongyu@outlook.com>
2020-10-19 17:02:46 +0800

    refactor 用数组取代大量的 if-else

--- src/main/java/mineSweeper/bean/MineImage.java
+++ src/main/java/mineSweeper/bean/MineImage.java
@@ -10,48 +10,33 @@ import javax.swing.ImageIcon;
  * @Description
  */
 public class MineImage {
+	private static final String[] IMAGE_PATHS;
+
+	static {
+		IMAGE_PATHS = new String[] {
+			"src/main/java/mineSweeper/images/m0.png",
+			"src/main/java/mineSweeper/images/m1.png",
+			"src/main/java/mineSweeper/images/m2.png",
+			"src/main/java/mineSweeper/images/m3.png",
+			"src/main/java/mineSweeper/images/m4.png",
+			"src/main/java/mineSweeper/images/m5.png",
+			"src/main/java/mineSweeper/images/m6.png",
+			"src/main/java/mineSweeper/images/m7.png",
+			"src/main/java/mineSweeper/images/m8.png",
+			"src/main/java/mineSweeper/images/m9.png",
+			"src/main/java/mineSweeper/images/blank.png",
+			"src/main/java/mineSweeper/images/flag.png",
+			"src/main/java/mineSweeper/images/unknow.png",
+			"src/main/java/mineSweeper/images/mine_click.png",
+			"src/main/java/mineSweeper/images/facedead.png",
+			"src/main/java/mineSweeper/images/facesmile.png",
+			"src/main/java/mineSweeper/images/facewin.png",
+			"src/main/java/mineSweeper/images/logo.png"
+		};
+	}
 
 	public static ImageIcon getImageIcon(int width, int height, int mineStatus) {
-		String imagePath = null;
-		if (mineStatus == MineType.MINE_STATUS_OPEN_0) {
-			imagePath = "src/main/java/mineSweeper/images/m0.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_1) {
-			imagePath = "src/main/java/mineSweeper/images/m1.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_2) {
-			imagePath = "src/main/java/mineSweeper/images/m2.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_3) {
-			imagePath = "src/main/java/mineSweeper/images/m3.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_4) {
-			imagePath = "src/main/java/mineSweeper/images/m4.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_5) {
-			imagePath = "src/main/java/mineSweeper/images/m5.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_6) {
-			imagePath = "src/main/java/mineSweeper/images/m6.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_7) {
-			imagePath = "src/main/java/mineSweeper/images/m7.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_8) {
-			imagePath = "src/main/java/mineSweeper/images/m8.png";
-		} else if (mineStatus == MineType.MINE_STATUS_OPEN_9) {
-			imagePath = "src/main/java/mineSweeper/images/m9.png";
-		} else if (mineStatus == MineType.MINE_STATUS_BLANK) {
-			imagePath = "src/main/java/mineSweeper/images/blank.png";
-		} else if (mineStatus == MineType.MINE_STATUS_FLAG) {
-			imagePath = "src/main/java/mineSweeper/images/flag.png";
-		} else if (mineStatus == MineType.MINE_STATUS_UNKNOW) {
-			imagePath = "src/main/java/mineSweeper/images/unknow.png";
-		} else if (mineStatus == MineType.MINE_STATUS_MINE_CLICK) {
-			imagePath = "src/main/java/mineSweeper/images/mine_click.png";
-		} else if (mineStatus == MineType.MINE_STATUS_DEAD) {
-			imagePath = "src/main/java/mineSweeper/images/facedead.png";
-		} else if (mineStatus == MineType.MINE_STATUS_MILE) {
-			imagePath = "src/main/java/mineSweeper/images/facesmile.png";
-		} else if (mineStatus == MineType.MINE_STATUS_WAIT) {
-			imagePath = "src/main/java/mineSweeper/images/facewait.png";
-		} else if (mineStatus == MineType.MINE_STATUS_WIN) {
-			imagePath = "src/main/java/mineSweeper/images/facewin.png";
-		} else if (mineStatus == MineType.MINE_STATUS_LOGO) {
-			imagePath = "src/main/java/mineSweeper/images/logo.png";
-		}
+		String imagePath = IMAGE_PATHS[mineStatus];
 
 		if (imagePath == null || "".equals(imagePath)) {
 			return null;
